6b1235f18f720225681134ecb53f1d047650000b
chris-at-589764937648 <chris.m.dietz@gmail.com>
2024-11-02 16:48:53 -0400

    Splitting class

---
+++ src/base_cloudwatch_log_handler.py
@@ -0,0 +1,77 @@
+
+import logging
+import os
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+## This class is a special log handler that will send logs to CloudWatch Logs that you specify in the constructor.
+## It will also send logs to the console, so you can see them in the AWS Lambda console.
+## Example usage:
+## logger = logging.getLogger()
+## logger.setLevel(logging.INFO)    
+## logger.addHandler(CloudWatchLogHandler('log-group-name', 'log-stream-name'))
+## logger.info('Hello, CloudWatch Logs!')
+class BaseCloudWatchLogHandler(logging.StreamHandler):
+    def __init__(self, log_group_name, log_stream_name, logsClient):
+        super().__init__()
+        self.log_group_name = log_group_name
+        self.log_stream_name = log_stream_name
+        self.sequence_token = None
+        self.client = logsClient
+
+    ## This will attempt to create a log group and return it.
+    ## If the log group already exists then it will simply return it.
+    def getOrCreateLogGroup(self):
+        # first attempt to creat the log group. It'll throw an exception if it already exists.
+        try:
+            self.client.create_log_group(logGroupName=self.log_group_name)
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ResourceAlreadyExistsException':
+                # The log group already exists so simply return it now.
+                pass
+            else:
+                # Else some other error occurred so re-raise the exception.
+                raise e
+        return self.client.describe_log_groups(logGroupNamePrefix=self.log_group_name)['logGroups'][0]
+    
+    def getOrCreateLogStream(self):
+        # first attempt to creat the log stream. It'll throw an exception if it already exists.
+        try:
+            self.client.create_log_stream(logGroupName=self.log_group_name, logStreamName=self.log_stream_name)
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ResourceAlreadyExistsException':
+                # The log stream already exists so simply return it now.
+                pass
+            elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+                # The log group doesn't exist so create it now then try to create the log stream again.
+                self.getOrCreateLogGroup()
+                # Now try to create the log stream again.
+                return self.getOrCreateLogStream()
+            else:
+                # Else some other error occurred so re-raise the exception.
+                raise e
+        return self.client.describe_log_streams(logGroupName=self.log_group_name, logStreamNamePrefix=self.log_stream_name)['logStreams'][0]
+
+    def sendLogEvents(self, logEvents):
+        try:
+            self.client.put_log_events(
+                logGroupName=self.log_group_name,
+                logStreamName=self.log_stream_name,
+                logEvents=logEvents,
+                sequenceToken="These are ignored now"
+            )
+        except ClientError as e:
+            # Check if the log stream exists and then create it if not. Then try to send the log again.
+            if e.response['Error']['Code'] == 'ResourceNotFoundException':
+                self.getOrCreateLogStream()
+                self.sendLogEvents(logEvents)
+            else:
+                # Else some other error occurred so re-raise the exception. There's nothign we can do.
+                print(e)
+        except Exception as e:
+            # Something categorically went wrong so print the exception.
+            print(e)
+
+    def emit(self, record):
+        raise NotImplementedError("You must implement this method in a subclass")
--- src/cloudwatch_log_handler.py
+++ src/cloudwatch_log_handler.py
@@ -1,9 +1,4 @@
-
-import logging
-import os
-import json
-import boto3
-from botocore.exceptions import ClientError
+from src.base_cloudwatch_log_handler import BaseCloudWatchLogHandler
 
 ## This class is a special log handler that will send logs to CloudWatch Logs that you specify in the constructor.
 ## It will also send logs to the console, so you can see them in the AWS Lambda console.
@@ -12,68 +7,18 @@ from botocore.exceptions import ClientError
 ## logger.setLevel(logging.INFO)    
 ## logger.addHandler(CloudWatchLogHandler('log-group-name', 'log-stream-name'))
 ## logger.info('Hello, CloudWatch Logs!')
-class CloudWatchLogHandler(logging.StreamHandler):
+class CloudWatchLogHandler(BaseCloudWatchLogHandler):
     def __init__(self, log_group_name, log_stream_name, logsClient):
-        super().__init__()
-        self.log_group_name = log_group_name
-        self.log_stream_name = log_stream_name
-        self.sequence_token = None
-        self.client = logsClient
-
-    ## This will attempt to create a log group and return it.
-    ## If the log group already exists then it will simply return it.
-    def getOrCreateLogGroup(self):
-        # first attempt to creat the log group. It'll throw an exception if it already exists.
-        try:
-            self.client.create_log_group(logGroupName=self.log_group_name)
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'ResourceAlreadyExistsException':
-                # The log group already exists so simply return it now.
-                pass
-            else:
-                # Else some other error occurred so re-raise the exception.
-                raise e
-        return self.client.describe_log_groups(logGroupNamePrefix=self.log_group_name)['logGroups'][0]
-    
-    def getOrCreateLogStream(self):
-        # first attempt to creat the log stream. It'll throw an exception if it already exists.
-        try:
-            self.client.create_log_stream(logGroupName=self.log_group_name, logStreamName=self.log_stream_name)
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'ResourceAlreadyExistsException':
-                # The log stream already exists so simply return it now.
-                pass
-            elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-                # The log group doesn't exist so create it now then try to create the log stream again.
-                self.getOrCreateLogGroup()
-                # Now try to create the log stream again.
-                return self.getOrCreateLogStream()
-            else:
-                # Else some other error occurred so re-raise the exception.
-                raise e
-        return self.client.describe_log_streams(logGroupName=self.log_group_name, logStreamNamePrefix=self.log_stream_name)['logStreams'][0]
+        super().__init__(log_group_name, log_stream_name, logsClient)
 
     def emit(self, record):
+        logEvents = [{
+            'timestamp': int(record.created * 1000),
+            'message': self.format(record)
+        }]
         try:
-            response = self.client.put_log_events(
-                logGroupName=self.log_group_name,
-                logStreamName=self.log_stream_name,
-                logEvents=[
-                    {
-                        'timestamp': int(record.created * 1000),
-                        'message': self.format(record)
-                    }
-                ],
-                sequenceToken="These are ignored nowe"
-            )
-        except ClientError as e:
-            # Check if the log stream exists and then create it if not. Then try to send the log again.
-            if e.response['Error']['Code'] == 'ResourceNotFoundException':
-                self.getOrCreateLogStream()
-                self.emit(record)
-            else:
-                # Else some other error occurred so re-raise the exception. There's nothign we can do.
-                print(e)
+            self.sendLogEvents(logEvents)
         except Exception as e:
-            # Something categorically went wrong so print the exception.
-            print(e)
+            # Something we can't recover from so just print to console and continue. 
+            # We don't want to crash the entire function because of a logging error.
+            print(f"Failed to send log events to CloudWatch Logs: {e}")
